18e87131ebaa9e8be216278e0a04c26b78163320
Maruu-H <mauraruminothernandez@gmail.com>
2024-06-14 19:23:15 -0400

    agregado de django admin

--- PatitasLove3/settings.py
+++ PatitasLove3/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'patitasLove'
+    'patitasLove',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- patitasLove/models.py
+++ patitasLove/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    marca = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to="producto", null=True)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.marca}) - ${self.precio}"
